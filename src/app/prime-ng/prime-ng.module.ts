d7837d4093a19d82153b9adae1e10066a426781d
Leonardo Puebla <leonardo38361@gmail.com>
2023-02-27 19:45:35 -0300

    PrimeNg module

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    ButtonModule,
+    CardModule
+  ]
+})
+export class PrimeNgModule { }
