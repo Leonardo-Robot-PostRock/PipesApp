c04a90d8ee47fecd5b0a5d003093677c9f56c135
Leonardo Puebla <leonardo38361@gmail.com>
2023-02-26 22:07:25 -0300

    introduccion a pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  nombre: string = 'leonardo puebla';
+  valor: number = 1220;
+  obj = {
+    nombre: 'Leonardo'
+  }
+
+  mostrarNombre() {
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+  }
 }
