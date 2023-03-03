b06e45577ba8fbe5c5ca8ce7fcb536ce7df51294
Leonardo Puebla <leonardo38361@gmail.com>
2023-03-03 01:19:35 -0300

    MenubarModule

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
-
-
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
   declarations: [],
-  exports: [
-    ButtonModule,
-    CardModule
-  ]
+  exports: [ButtonModule, CardModule, MenubarModule]
 })
-export class PrimeNgModule { }
+export class PrimeNgModule {}
