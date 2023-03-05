e67c713d04404a90d445303569362283af673beb
Leonardo Puebla <leonardo38361@gmail.com>
2023-03-04 21:27:08 -0300

    Common module

--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
@@ -23,7 +24,8 @@ import { OrdenarComponent } from './pages/ordenar/ordenar.component';
     OrdenarComponent
   ],
   imports: [
-    PrimeNgModule
+    PrimeNgModule,
+    CommonModule
   ]
 })
 export class VentasModule { }
