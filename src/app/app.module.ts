08d34c6f5482e242062e66b0c7da6491fb559a02
Leonardo Puebla <leonardo38361@gmail.com>
2023-03-15 17:22:14 -0300

    tokens para configuración regional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,19 @@ import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
 
 //Cambiar el locale de la app
-import localeEs from '@angular/common/locales/es-AR';
+import localeEsAr from '@angular/common/locales/es-AR';
+import localeFr from '@angular/common/locales/fr';
+import localeEsHn from '@angular/common/locales/es-HN';
 import { registerLocaleData } from '@angular/common';
-registerLocaleData(localeEs);
+registerLocaleData(localeEsAr);
+registerLocaleData(localeFr);
+registerLocaleData(localeEsHn);
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRouterModule, SharedModule, VentasModule],
   providers: [{
-    provide: LOCALE_ID, useValue: 'es-AR'
+    provide: LOCALE_ID, useValue: 'es-AR', 
   }],
   bootstrap: [AppComponent]
 })
