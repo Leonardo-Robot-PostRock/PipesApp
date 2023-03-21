fe705ba2425ef8564feff13b2e47e8e0d3e72f87
Leonardo Puebla <leonardo38361@gmail.com>
2023-03-20 22:35:24 -0300

    import the fieldSetModule of primeNg

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CardModule } from 'primeng/card';
+
+//PrimeNg
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import {FieldsetModule} from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
+
 
 @NgModule({
   declarations: [],
-  exports: [ButtonModule, CardModule, MenubarModule]
+  exports: [ButtonModule, CardModule, MenubarModule, FieldsetModule]
 })
 export class PrimeNgModule {}
