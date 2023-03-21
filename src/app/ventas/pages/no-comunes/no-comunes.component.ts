ac3c75314c7655af94443a837ee8061881f83e15
Leonardo Puebla <leonardo38361@gmail.com>
2023-03-21 20:07:39 -0300

    names in english and add variables for the example i18nPlural pipe. Also add types of typescript

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -3,14 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
-  styles: [
-  ]
+  styles: []
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
+  // i18nSelect
+  name: string = 'Malala Yousafzai';
+  gender: string = 'femenino';
 
-  constructor() { }
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  };
 
-  ngOnInit(): void {
-  }
-
+  // i18nPlural
+  customer: string[] = ['Maria', 'Norberto', 'Perez'];
+  customersMap: { [k: string]: string } = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos 2 clientes esperando',
+    other: 'tenemos # clientes esperando'
+  };
 }
