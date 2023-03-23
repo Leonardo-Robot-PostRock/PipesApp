e79073c003ea30a44201b6a6515e1422a472ed87
Leonardo Puebla <leonardo38361@gmail.com>
2023-03-22 22:19:16 -0300

    route custom pipe

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -19,13 +19,13 @@ export class MenuComponent implements OnInit {
         items: [
           { label: 'Texts and dates', icon: 'pi pi-calendar-plus', routerLink: '/' },
           { label: 'Numbers', icon: 'pi pi-dollar', routerLink: 'numbers' },
-          { label: 'No commons', icon: 'pi pi-globe', routerLink: 'no-common' },
-          { label: 'Order', icon: 'pi pi-sort-alpha-up-alt', routerLink: 'order' }
+          { label: 'No commons', icon: 'pi pi-globe', routerLink: 'no-common' }
         ]
       },
       {
-       label: 'Custom Pipes',
-       icon: 'pi pi-cog' 
+        label: 'Custom Pipes',
+        icon: 'pi pi-cog',
+        routerLink: 'order'
       }
     ];
   }
