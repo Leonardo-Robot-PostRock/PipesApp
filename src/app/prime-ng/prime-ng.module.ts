5e6ca0fffe0a58762e7325c29867b19b7418eabd
Leonardo Puebla <leonardo38361@gmail.com>
2023-03-26 01:46:25 -0300

    Table and Toolbar module

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 //PrimeNg
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
-import {FieldsetModule} from 'primeng/fieldset';
+import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
-
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @NgModule({
   declarations: [],
-  exports: [ButtonModule, CardModule, MenubarModule, FieldsetModule]
+  exports: [ButtonModule, CardModule, FieldsetModule, MenubarModule, TableModule, ToolbarModule]
 })
 export class PrimeNgModule {}
