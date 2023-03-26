853245c0a345b4a6fe78d980a34494e3cbd6286a
Leonardo Puebla <leonardo38361@gmail.com>
2023-03-26 04:41:18 -0300

    creacion de objeto heroes con su tipado

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,16 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Colour, Heroe } from '../../interfaces/ventas.interefaces';
 
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html',
   styles: []
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
   inCapitalLetter: boolean = true;
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  heroes: Heroe[] = [
+    {
+      name: 'Superman',
+      fly: true,
+      colour: Colour.azul
+    },
+    {
+      name: 'Batman',
+      fly: false,
+      colour: Colour.negro
+    },
+    {
+      name: 'Robin',
+      fly: false,
+      colour: Colour.verde
+    },
+    {
+      name: 'Daredevil',
+      fly: false,
+      colour: Colour.rojo
+    },
+    {
+      name: 'Linterna Verde',
+      fly: true,
+      colour: Colour.verde
+    }
+  ];
 
   changeLetters() {
     this.inCapitalLetter = !this.inCapitalLetter;
